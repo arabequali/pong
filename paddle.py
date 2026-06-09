e6d8e87d231ea401e9de5afc72181fd26cb8fe2d
avally <vallyanil24@gmail.com>
2026-06-09 21:30:08 +0000

    Paddle x, y and size values rounded with //

--- paddle.py
+++ paddle.py
@@ -6,11 +6,15 @@ class Paddle:
     def __init__(self, player: int, screen: Surface) -> None:
         self.screen = screen
         self.screen_size = pygame.display.get_window_size()
-        self.size_x = self.screen_size[0] / 80
-        self.size_y = self.screen_size[1] / 10
+        self.size_x = self.screen_size[0] // 80
+        self.size_y = self.screen_size[1] // 10
         if player == 1:
             self.x = 3 * self.size_x
-            self.y = self.screen_size[1] / 2 - self.size_y / 2
+            self.y = self.screen_size[1] // 2 - self.size_y // 2
+        else:
+            self.x = 0
+            self.y = 0
+        self.player_hitbox = pygame.Rect(self.x, self.y, self.size_x, self.size_y)
 
 
     def go_up(self) -> None:
